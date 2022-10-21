f4fdda0672d82ae46c04ea4d108d28ba00d72d70
KGTH <kgthcruz94@gmail.com>
2022-10-21 11:59:57 +0000

    Se crea excusas aleatorias

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let aleatoriowho = Math.floor(Math.random() * who.length);
+  let aleatoriowhat = Math.floor(Math.random() * what.length);
+  let aleatoriowhen = Math.floor(Math.random() * when.length);
+
+  const elemento = document.querySelector("#excuse");
+  elemento.innerHTML = `${who[aleatoriowhat]} ${what[aleatoriowhen]} ${when[aleatoriowho]}`;
 };
+
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
