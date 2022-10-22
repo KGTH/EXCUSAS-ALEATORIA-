8f9082f7592a5cad1f51f86c0ebb2c0555485f2a
KGTH <kgthcruz94@gmail.com>
2022-10-22 14:03:43 +0000

    se actualiza trabajo

--- src/app.js
+++ src/app.js
@@ -5,21 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
+window.onload = function() {
+  let elemento = document.querySelector(".excuse");
+  elemento.innerHTML = aleatorio();
+};
+
+let aleatorio = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
   let aleatoriowho = Math.floor(Math.random() * who.length);
   let aleatoriowhat = Math.floor(Math.random() * what.length);
   let aleatoriowhen = Math.floor(Math.random() * when.length);
 
-  const elemento = document.querySelector("#excuse");
-  elemento.innerHTML = `${who[aleatoriowhat]} ${what[aleatoriowhen]} ${when[aleatoriowho]}`;
+  return `${who[aleatoriowhat]} ${what[aleatoriowhen]} ${when[aleatoriowho]}`;
 };
-
-let who = ["the dog", "my granma", "his turtle", "my bird"];
-let what = ["eat", "pissed", "crushed", "broked"];
-let when = [
-  "before the class",
-  "right in time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
